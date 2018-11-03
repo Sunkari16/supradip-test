1eb58e8135b89ba280dbca8994498205c9bd7291
Hari <harikrishnasunkari@gmail.com>
2018-11-03 17:08:55 +0530

    first commit test

---
+++ app/src/main/java/test/supradip/test/MainActivity.java
@@ -0,0 +1,25 @@
+package test.supradip.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.tb_main);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void supportInvalidateOptionsMenu() {
+        super.supportInvalidateOptionsMenu();
+    }
+}
